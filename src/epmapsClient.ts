b404e068075094bfe25e64cc02f0e39b1b54c34f
stalincrisanto <stalincrisanto@hotmail.com>
2025-03-19 23:00:14 -0500

    feat: add method for update data of user in sdp

--- src/epmapsClient.ts
+++ src/epmapsClient.ts
@@ -1,19 +1,22 @@
 import { createClientAsync } from "soap";
+import { UserEpmap } from "./types";
 
 const SOAP_URL = "http://localhost:3001/soap?wsdl";
 
 export const getInfoUserEmpams = async (email: string) => {
-    try {
-        const client = await createClientAsync(SOAP_URL);
-        const args = {
-          CORREO_EMPLEADO: email,
-        };
-        const response = await client.MT_EstrucOrgEmpleados_ReqAsync(args);
-      
-        console.log("response en api soap", response[0].DATOS);
-    } catch (error) {
-        console.log("error", error);
-    }
+  try {
+    const client = await createClientAsync(SOAP_URL);
+    const args = {
+      CORREO_EMPLEADO: email,
+    };
+    const response = await client.MT_EstrucOrgEmpleados_ReqAsync(args);
+
+    const user: UserEpmap = response[0].DATOS;
+    console.log("response en api soap", response[0].DATOS);
+    return user;
+  } catch (error) {
+    console.log("error", error);
+  }
 };
 
 // import { createClientAsync, BasicAuthSecurity } from "soap";
--- src/serviceDeskClient.ts
+++ src/serviceDeskClient.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 import https from "https";
-import type { ResponseSD } from "./types";
+import type { ResponseSD, UserEpmap } from "./types";
 import { logger } from "./utils/logger";
 import fs from "fs";
+import { getInfoUserEmpams } from "./epmapsClient";
 
 const inputData = `{
   "list_info": {
@@ -13,10 +14,10 @@ const inputData = `{
 
 const encodedInputData = encodeURIComponent(inputData);
 
-
 const URL_API = `https://localhost:8080/api/v3/users`;
 const URL_WITH_PARAMS = `${URL_API}?input_data=${encodedInputData}`;
-const API_KEY_SERVICEDESK = "A2C6B0D6-CE8F-4D22-A61D-7A0C81954266";
+// const API_KEY_SERVICEDESK = "A2C6B0D6-CE8F-4D22-A61D-7A0C81954266"; //machine in metro
+const API_KEY_SERVICEDESK = "2A61B73F-5C50-4C45-B022-B6471454E007"; //my computer
 const OUTPUT_FILE = "D:\\result.json";
 // const URL_API = "https://srvsdeskprd:8080/api/v3/users";
 // const API_KEY_SERVICEDESK = "B632A057-CD5F-4F07-ACDE-22433C9A4063";
@@ -41,7 +42,37 @@ export const getUsersFromServiceDesk = async () => {
       name,
     }));
 
-    console.log("response", allUsersEmails);
+    const userTest = allUsersEmails.find(
+      ({ email_id }) => email_id === "stalincrisi13@gmail.com"
+    );
+
+    const dataFromEpmaps: UserEpmap | undefined = await getInfoUserEmpams(
+      userTest?.email_id!
+    );
+
+    console.log("---->", dataFromEpmaps);
+
+    let data = JSON.stringify({
+      input_data: `{"user":{"jobtitle":"${dataFromEpmaps?.ZTPLANS_JF}"}}`,
+    });
+
+    const responseUpdate = await axios.put(
+      `${URL_API}/${userTest?.id}`,
+      new URLSearchParams({
+        'input_data': JSON.stringify({
+          user: { 
+            jobtitle: dataFromEpmaps?.ZTPLANS
+          }
+        })
+      }),
+      {
+        headers: {
+          authtoken: API_KEY_SERVICEDESK,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        httpsAgent,  // Este valor debe estar definido previamente
+      }
+    );
 
     // fs.writeFileSync(OUTPUT_FILE, JSON.stringify(allUsersEmails, null, 2));
     // logger.info(`Resultado guardado en ${OUTPUT_FILE}`);
