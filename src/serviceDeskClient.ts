eb0857752f5f0990f78a75150be475d984d75885
stalincrisanto <stalincrisanto@hotmail.com>
2025-03-20 17:00:44 -0500

    feat: many updates

--- src/serviceDeskClient.ts
+++ src/serviceDeskClient.ts
@@ -16,8 +16,8 @@ const encodedInputData = encodeURIComponent(inputData);
 
 const URL_API = `https://localhost:8080/api/v3/users`;
 const URL_WITH_PARAMS = `${URL_API}?input_data=${encodedInputData}`;
-// const API_KEY_SERVICEDESK = "A2C6B0D6-CE8F-4D22-A61D-7A0C81954266"; //machine in metro
-const API_KEY_SERVICEDESK = "2A61B73F-5C50-4C45-B022-B6471454E007"; //my computer
+const API_KEY_SERVICEDESK = "A2C6B0D6-CE8F-4D22-A61D-7A0C81954266"; //machine in metro
+// const API_KEY_SERVICEDESK = "2A61B73F-5C50-4C45-B022-B6471454E007"; //my computer
 const OUTPUT_FILE = "D:\\result.json";
 // const URL_API = "https://srvsdeskprd:8080/api/v3/users";
 // const API_KEY_SERVICEDESK = "B632A057-CD5F-4F07-ACDE-22433C9A4063";
@@ -25,6 +25,7 @@ const OUTPUT_FILE = "D:\\result.json";
 
 export const getUsersFromServiceDesk = async () => {
   logger.info("INICIANDO PROCESO SERVICE DESK....");
+
   try {
     const httpsAgent = new https.Agent({ rejectUnauthorized: false });
     const responseFromApi = await axios.get(URL_WITH_PARAMS, {
@@ -42,37 +43,70 @@ export const getUsersFromServiceDesk = async () => {
       name,
     }));
 
-    const userTest = allUsersEmails.find(
-      ({ email_id }) => email_id === "stalincrisi13@gmail.com"
-    );
-
-    const dataFromEpmaps: UserEpmap | undefined = await getInfoUserEmpams(
-      userTest?.email_id!
-    );
-
-    console.log("---->", dataFromEpmaps);
-
-    let data = JSON.stringify({
-      input_data: `{"user":{"jobtitle":"${dataFromEpmaps?.ZTPLANS_JF}"}}`,
+    const promises = allUsersEmails.map(({ id, email_id }) => {
+      if (email_id) {
+        return getInfoUserEmpams(email_id).then(async (userFromEpmap) => {
+          const responseUpdate = await axios.put(
+            `${URL_API}/${id}`,
+            new URLSearchParams({
+              input_data: JSON.stringify({
+                user: {
+                  jobtitle: userFromEpmap?.ZTPLANS,
+                },
+              }),
+            }),
+            {
+              headers: {
+                authtoken: API_KEY_SERVICEDESK,
+                "Content-Type": "application/x-www-form-urlencoded",
+              },
+              httpsAgent,
+            }
+          );
+          return responseUpdate;
+        });
+      }
+      return Promise.resolve(); // Si no hay email_id, no hace nada
     });
 
-    const responseUpdate = await axios.put(
-      `${URL_API}/${userTest?.id}`,
-      new URLSearchParams({
-        'input_data': JSON.stringify({
-          user: { 
-            jobtitle: dataFromEpmaps?.ZTPLANS
-          }
-        })
-      }),
-      {
-        headers: {
-          authtoken: API_KEY_SERVICEDESK,
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        httpsAgent,  // Este valor debe estar definido previamente
-      }
-    );
+    // Solo ejecutamos Promise.all si hay promesas
+    if (promises.length > 0) {
+      await Promise.all(promises);
+    }
+
+    // console.log("----->", await Promise.all(promises));
+
+    // const userTest = allUsersEmails.find(
+    //   ({ email_id }) => email_id === "stalincrisi13@gmail.com"
+    // );
+
+    // const dataFromEpmaps: UserEpmap | undefined = await getInfoUserEmpams(
+    //   userTest?.email_id!
+    // );
+
+    // console.log("---->", dataFromEpmaps);
+
+    // let data = JSON.stringify({
+    //   input_data: `{"user":{"jobtitle":"${dataFromEpmaps?.ZTPLANS_JF}"}}`,
+    // });
+
+    // const responseUpdate = await axios.put(
+    //   `${URL_API}/${userTest?.id}`,
+    //   new URLSearchParams({
+    //     'input_data': JSON.stringify({
+    //       user: {
+    //         jobtitle: dataFromEpmaps?.ZTPLANS
+    //       }
+    //     })
+    //   }),
+    //   {
+    //     headers: {
+    //       authtoken: API_KEY_SERVICEDESK,
+    //       'Content-Type': 'application/x-www-form-urlencoded',
+    //     },
+    //     httpsAgent,  // Este valor debe estar definido previamente
+    //   }
+    // );
 
     // fs.writeFileSync(OUTPUT_FILE, JSON.stringify(allUsersEmails, null, 2));
     // logger.info(`Resultado guardado en ${OUTPUT_FILE}`);
